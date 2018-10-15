c52357d5aeddcf3560d0fac01dc85c42c7618717
Aakash Sarang <sarangaakash@gmail.com>
2018-10-15 14:36:43 -0700

    Add configuration for scss files

--- question1/webpack.config.js
+++ question1/webpack.config.js
@@ -1,7 +1,35 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
     entry: './index.js',
     output: {
-        filename: '[question1].[hash].bundle.es5.js'
+        filename: '[hash].bundle.es5.js'
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.scss$/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[hash].css"
+        })
+    ],
+    module: {
+        rules: [{
+            test: [/\.scss$/,/\.css$/],
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader"
+            ]
+        }]
     },
     resolve: {
         extensions: ["js", ".jsx", "*"]
