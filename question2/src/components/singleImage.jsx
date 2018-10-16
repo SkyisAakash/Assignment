4a6450fd6c09247d46fcc9f0091a43489a068f8c
Aakash Sarang <sarangaakash@gmail.com>
2018-10-15 18:26:04 -0700

    Write styles for singleImage

--- question2/src/components/singleImage.jsx
+++ question2/src/components/singleImage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CSSTransition } from 'react-transition-group';
-
+import './singleImage.css';
 const SingleImage = (props) => {
     const {url, center, index, caption} = props;
     return (
@@ -10,7 +10,7 @@ const SingleImage = (props) => {
             appear={true}
             classNames='fade'>
         <div key='photo' className={'photo-container'}>
-            <img src={url} key={index} />
+            <img src={url} key={index} className={'image'} />
             <p>{caption}</p>
             {center ? <p>I am centered</p> : null}
         </div>
