cb892dcbef8f61693b745b5b0e8ee84757e9efc3
Aakash Sarang <sarangaakash@gmail.com>
2018-10-15 17:54:37 -0700

    Create list of images

--- question2/src/components/gallery.js
+++ question2/src/components/gallery.js
@@ -8,6 +8,8 @@ class PhotoGallery extends React.Component{
         this.state = {
             index: 0
         }
+        this.moveLeft = this.moveLeft.bind(this)
+        this.moveRight = this.moveRight.bind(this)
     }
 
     moveLeft() {
@@ -22,11 +24,33 @@ class PhotoGallery extends React.Component{
         })
     }
 
+    UNSAFE_componentWillReceiveProps(newProps) {
+        if (newProps.images.length - 1 < this.state.index) {
+            this.setState({
+                index: 0
+            })
+        }
+    }
+
     render() {
+        let { index } = this.state;
+        const { images } = this.props;
         return(
             <div className='carousel' tabIndex='0'>
                 <ul className='carousel-wrap'>
-                    {/* images */}
+                    { images[idx-1] ? (
+                        <li className='right-li' onClick={this.moveLeft}>
+                            {/* image-1 */}
+                        </li>
+                    ) : (<li className='empty'></li>)}
+                    <li className='center-li'>
+                        {/* image0 */}
+                    </li>
+                    {images[idx + 1] ? (
+                        <li className='left-li' onClick={this.moveRight}>
+                            {/* image1 */}
+                        </li>
+                    ) : (<li className='empty'></li>)}
                 </ul>
             </div>
         )
