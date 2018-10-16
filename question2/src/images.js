ea621709c5256873b956a6f24e425d9b1af7a0e0
Aakash Sarang <sarangaakash@gmail.com>
2018-10-15 17:22:55 -0700

    Start writing component

---
+++ question2/src/images.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+
+class Images extends React.Component{
+    render() {
+        return(
+            <h1>This is Images</h1>
+        )
+    }
+}
+
+export default Images;
+
